7bcd0c8f76e3e6460a56de54cbfc43d370fd9b80
Khushi <khushijain1523@gmail.com>
2024-03-17 14:57:29 +0530

    Generate HTML for order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,10 @@
-export const cart = []; // this variable can be used outside of this file
+export const cart = [{
+  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity: 2
+},{
+  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity: 1
+}]; 
 
 export function addToCart(productId){
   let matchingItem;
@@ -18,3 +24,5 @@ export function addToCart(productId){
       });
     }
 }
+
+// using export we can access that property or function outside the file
